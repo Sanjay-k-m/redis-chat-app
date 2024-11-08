98bb3aab3f0313bf1abcb8af0ea9cce4dde15c4c
sanjay-k-m <sanjay_k.m@outlook.com>
2024-11-08 20:38:58 +0530

    update preference  tab

--- src/components/PreferencesTab.tsx
+++ src/components/PreferencesTab.tsx
@@ -1,21 +1,28 @@
 "use client";
 import React from "react";
 import { Button } from "./ui/button";
-import { MoonIcon, SunIcon, Volume2 } from "lucide-react";
+import { MoonIcon, SunIcon, Volume2, VolumeX } from "lucide-react";
 import { useTheme } from "next-themes";
+import { usePreferences } from "@/store/usePreferences";
 
 const PreferencesTab = () => {
-    const {setTheme} = useTheme()
+  const { setTheme } = useTheme();
+
+  const { soundEnabled, setSoundEnabled } = usePreferences();
   return (
     <div className="flex flex-wrap gap-2 px-1 md:px-2">
-      <Button variant={"outline"} size={"icon"} onClick={() => setTheme("light")}>
-        <SunIcon className="size-[1.2rem] text-muted-foreground" />
+      <Button variant="outline" size="icon" onClick={() => setTheme("light")}>
+        <SunIcon className="w-[1.2rem] h-[1.2rem] text-muted-foreground" />
       </Button>
-      <Button variant={"outline"} size={"icon"} onClick={() => setTheme("dark")}>
-        <MoonIcon className="size-[1.2rem] text-muted-foreground" />
+      <Button variant="outline" size="icon" onClick={() => setTheme("dark")}>
+        <MoonIcon className="w-[1.2rem] h-[1.2rem] text-muted-foreground" />
       </Button>
-      <Button variant={"outline"} size={"icon"}>
-        <Volume2 className="size-[1.2rem] text-muted-foreground" />
+      <Button variant="outline" size="icon" onClick={() => setSoundEnabled(!soundEnabled)}>
+        {soundEnabled ? (
+          <Volume2 className="w-[1.2rem] h-[1.2rem] text-muted-foreground" />
+        ) : (
+          <VolumeX className="w-[1.2rem] h-[1.2rem] text-muted-foreground" />
+        )}
       </Button>
     </div>
   );
