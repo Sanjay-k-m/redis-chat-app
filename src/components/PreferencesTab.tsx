03198a4830dcb0c639d6b59682f43bfa30635a84
sanjay-k-m <sanjay_k.m@outlook.com>
2024-11-09 08:00:10 +0530

    layout done

--- src/components/PreferencesTab.tsx
+++ src/components/PreferencesTab.tsx
@@ -4,20 +4,37 @@ import { Button } from "./ui/button";
 import { MoonIcon, SunIcon, Volume2, VolumeX } from "lucide-react";
 import { useTheme } from "next-themes";
 import { usePreferences } from "@/store/usePreferences";
+import useSound from "use-sound";
 
 const PreferencesTab = () => {
   const { setTheme } = useTheme();
+  const [playMouseClick] = useSound("/sounds/mouse-click.mp3");
+  const [playSoundOn] = useSound("/sounds/sound-on.mp3", { volume: 0.3 });
+  const [playSoundOff] = useSound("/sounds/sound-off.mp3", { volume: 0.3 });
+
 
   const { soundEnabled, setSoundEnabled } = usePreferences();
   return (
     <div className="flex flex-wrap gap-2 px-1 md:px-2">
-      <Button variant="outline" size="icon" onClick={() => setTheme("light")}>
+      <Button
+        variant="outline"
+        size="icon"
+        onClick={() => {
+          setTheme("light");
+          soundEnabled && playMouseClick();
+        }}
+      >
         <SunIcon className="w-[1.2rem] h-[1.2rem] text-muted-foreground" />
       </Button>
-      <Button variant="outline" size="icon" onClick={() => setTheme("dark")}>
+      <Button variant="outline" size="icon" onClick={() => {
+          setTheme("dark");
+          soundEnabled && playMouseClick();
+        }}>
         <MoonIcon className="w-[1.2rem] h-[1.2rem] text-muted-foreground" />
       </Button>
-      <Button variant="outline" size="icon" onClick={() => setSoundEnabled(!soundEnabled)}>
+      <Button variant="outline" size="icon" onClick={() => {setSoundEnabled(!soundEnabled)
+        soundEnabled ? playSoundOff() : playSoundOn();
+      }}>
         {soundEnabled ? (
           <Volume2 className="w-[1.2rem] h-[1.2rem] text-muted-foreground" />
         ) : (
